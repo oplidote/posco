06e521207d3436e9ed58a2b0789420d5fe46b0e3
jhwmac <jkr2505@naver.com>
2022-04-06 15:07:28 +0900

    모달창 완료

--- js/script.js
+++ js/script.js
@@ -1,10 +1,21 @@
 $(document).ready(function () {
-
     // 모달창
     let $modal = $('.modal');
     let $modal_close = $('.modal-close');
-    $modal_close.click(function(){
+    let $comment_bt = $('.comment-bt');
+    $modal_close.click(function () {
+        $('html').css('overflow-y', 'auto');
         $modal.fadeOut(300);
+    });
+    $comment_bt.click(function () {
+
+        $(this).hide();
+        $(this).text('닫기').show(300);
+        if ($modal.hasClass('comment-on')) {
+            $(this).hide();
+            $(this).text('리뷰 및 개선사항').show(300);
+        }
+        $modal.toggleClass('comment-on');
     });
     // 페이징 바
 
